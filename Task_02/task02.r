2be3e88d4ed8d66f2a65e53c0b9596369ad30e9a
alexburdette88 <burdette_alexander@yahoo.com>
2020-01-28 17:59:20 -0500

    <Alexander Burdette> Task 03

--- Task_02/task02.r
+++ Task_02/task02.r
@@ -27,26 +27,54 @@ head(beren)
 head(beren2)
 head(beren3)
 write.csv(beren3,"beren_new.csv",quote=F,row.names=FALSE)
+
 #Question1: The first Hypothesis values dont correspond, the second hypothesis relationship is to vague.
+setwd()
+beren3 <- read.csv("beren_new.csv", stringsAsFactor=F)
+
 Feeds<-which(beren3$event=="bottle")
-avgMilk<-mean(beren$value[Feeds])
-avgFeeds<-tapply(beren3$value[Feeds],beren$age[Feeds],mean)
+avgMilk<-mean(beren3$value[Feeds])
+avgFeeds<-tapply(beren3$value[Feeds],beren3$age[Feeds],mean)
 avgFeeds
-varFeed<-tapply(beren3$value[Feeds],beren$age[Feeds],var)
-totalFeed<-tapply(beren3$value[Feeds],beren$age[Feeds],sum)
-numFeeds<-tapply(beren3$value[Feeds],beren$age[Feeds],length)
-cor(beren3$value[Feeds],beren$age[Feeds])
+
+varFeed<-tapply(beren3$value[Feeds],beren3$age[Feeds],var)
+totalFeed<-tapply(beren3$value[Feeds],beren3$age[Feeds],sum)
+numFeeds<-tapply(beren3$value[Feeds],beren3$age[Feeds],length)
+
+cor(beren3$value[Feeds],beren3$age[Feeds])
 cor.test(beren3$value[Feeds],beren3$age[Feeds])
 berenCor<-cor.test(beren3$value[Feeds],beren3$age[Feeds])
-summary(berenCor)
+berenCor
+
 berenANOVA<-aov(beren3$value[Feeds]~beren3$caregiver[Feeds])
+
 boxplot(beren3$value[Feeds]~beren3$caregiver[Feeds],xlab="who gave the bottle", ylab="amount of milk consumed (oz)")
+
 par(las=1,mar=c(5,5,1,1),mgp=c(2,0.5,0),tck=-0.01)
 plot(as.numeric(names(totalFeed)),totalFeed,type="b",pch=16,xlab="age in days",ylab="ounces of milk")
 abline(h=mean(totalFeed),lty=2,col='red')
+
 pdf("r02b-totalMilkByDay.pdf",height=4,width=4)
+
 par(las=1,mar=c(5,5,1,1),mgp=c(2,0.5,0),tck=-0.01)
 plot(as.numeric(names(totalFeed)),totalFeed,type="b",pch=16,xlab="age in days",ylab="ounces of milk")
 abline(h=mean(totalFeed),lty=2,col='red')
+
 dev.off()
 #question2: Because we dont have info on milk or not or frequency in daycare
+setwd()
+beren3 <- read.csv("beren_new.csv", stringsAsFactor=F)
+
+unique(beren3$event)
+Solids <- which(beren3$event == "intro_solidfood")
+Age <- beren3[Solids, "age"]
+
+Naps <- which(beren3$event == "nap")
+napData <- beren3[Naps,]
+head(napData)
+
+youngNaps <- napData[which(napData$age < Age),]
+
+
+
+
